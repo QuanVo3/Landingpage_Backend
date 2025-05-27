c449d8a10ef4e457b75b47c452d65dd5b7133c37
QuanVo3 <votrandongquan@gmail.com>
2025-05-27 11:27:58 +0700

    fixed cors

--- app.js
+++ app.js
@@ -11,12 +11,7 @@ const articleRoutes = require("./routes/article/index");
 // Tạo app
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 // Sử dụng routes
