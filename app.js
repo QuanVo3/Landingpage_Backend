cc168713d0c08d076b0ef980b720b0c1b8620bba
QuanVo3 <votrandongquan@gmail.com>
2025-05-29 09:35:24 +0700

    fixed cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const miniAppOptionsRoutes = require("./routes/MiniAppOptions/index");
 // Tạo app
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 
 // Sử dụng routes
--- routes/MiniAppOptions/index.js
+++ routes/MiniAppOptions/index.js
@@ -6,48 +6,53 @@ const router = express.Router();
 
 //Get MiniAppOptions
 router.get("/", async (req, res) => {
-    try {
-        const options = await MiniAppOptions.findOne();
-        if (!options) {
-            return res.status(404).json({ message: "Không tìm thấy cấu hình." });
-        }
-        res.json(options)
-    } catch (error) {
-        res.status(500).json({ message: "Lỗi server khi lấy cấu hình", error });
+  try {
+    const options = await MiniAppOptions.findOne();
+    if (!options) {
+      return res.status(404).json({ message: "Không tìm thấy cấu hình." });
     }
+    res.json(options);
+  } catch (error) {
+    res.status(500).json({ message: "Lỗi server khi lấy cấu hình", error });
+  }
 });
 
 //Put MiniAppOptions
-router.put("/", verifyToken, async (req, res) => {
-    try {
-        const updatedOptions = await MiniAppOptions.findOneAndUpdate(
-            {},
-            {
-                showFacebook: req.body.showFacebook,
-                showYouTube: req.body.showYouTube,
-                showIntroduction: req.body.showIntroduction
-            },
-            { new: true, upsert: true }
-        );
-        res.json(updatedOptions);
-    } catch (error) {
-        res.status(500).json({ message: "Lỗi server khi cập nhật cấu hình.", error });
-    }
+router.patch("/", verifyToken, async (req, res) => {
+  const { showFacebook, showYouTube, showIntroduction } = req.body;
+  try {
+    const updatedOptions = await MiniAppOptions.findOneAndUpdate(
+      {},
+      {
+        showFacebook: showFacebook,
+        showYouTube: showYouTube,
+        showIntroduction: showIntroduction,
+      },
+      { new: true, upsert: true }
+    );
+    res.json(updatedOptions);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Lỗi server khi cập nhật cấu hình.", error });
+  }
 });
 
 //Post MiniAppOptions
 router.post("/init", verifyToken, async (req, res) => {
-    try {
-        const existing = await MiniAppOptions.findOne();
-        if (existing) {
-            return res.status(400).json({ message: "Cấu hình đã tồn tại." });
-        }
-        const defaultOptions = new MiniAppOptions();
-        await defaultOptions.save();
-        res.status(201).json(defaultOptions);
-    } catch (error) {
-        res.status(500).json({ message: "Lỗi khi khởi tạo cấu hình mặc định. ", error })
+  try {
+    const existing = await MiniAppOptions.findOne();
+    if (existing) {
+      return res.status(400).json({ message: "Cấu hình đã tồn tại." });
     }
+    const defaultOptions = new MiniAppOptions();
+    await defaultOptions.save();
+    res.status(201).json(defaultOptions);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Lỗi khi khởi tạo cấu hình mặc định. ", error });
+  }
 });
 
 module.exports = router;
